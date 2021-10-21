357058ebb97860ed3ad4481b655c5f19baa9632d
Lucca Barroso <barrosolucca@gmail.com>
2021-10-21 14:29:25 -0300

    3d vector visualization

---
+++ src/components/ViewPort.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="viewport">
+
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapActions } from "vuex";
+export default {
+    data () {
+    return {
+      height: 0
+    };
+  },
+  methods: {
+    ...mapMutations(["RESIZE"]),
+    ...mapActions(["INIT", "ANIMATE"])
+  },
+  mounted () {
+    this.INIT({
+      width: this.$el.offsetWidth,
+      height: this.$el.offsetHeight,
+      el: this.$el
+    }).then(() => {
+      this.ANIMATE();
+      window.addEventListener("resize", () => {
+        this.RESIZE({
+          width: this.$el.offsetWidth,
+          height: this.$el.offsetHeight
+        });
+      });
+    });
+  }
+};
+</script>
+
+<style>
+    .viewport{
+        padding-top: 60px;
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
--- src/views/threeD.vue
+++ src/views/threeD.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <view-port />
     <plane-three-d style="height:100vh" />
     <display-position :twoD="false" />
     <divider />
@@ -9,11 +10,12 @@
 
 <script>
 import ChangingFunctions from "../components/ChangingFunctions.vue";
+import ViewPort from "../components/ViewPort.vue"
 import DisplayPosition from "../components/DisplayPosition.vue";
 import Divider from "../components/Divider.vue";
 import PlaneThreeD from "../components/PlaneThreeD.vue";
 export default {
-  components: { Divider, ChangingFunctions, DisplayPosition, PlaneThreeD },
+  components: { Divider, ChangingFunctions, DisplayPosition, PlaneThreeD, ViewPort },
   name: "threeD",
 };
 </script>
